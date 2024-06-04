c0f43d199f86d52c759735405f1afccd87ec1653
likui628 <90845831+likui628@users.noreply.github.com>
2024-06-04 21:37:10 +0800

    init project

---
+++ src/test/java/aliyun/oss/AliyunOssApplicationTests.java
@@ -0,0 +1,13 @@
+package aliyun.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyunOssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
